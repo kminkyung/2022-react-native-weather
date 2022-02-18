d64c95710380ac4d5693bbc96e124d89b1aae883
Minkyung <mminkyung.k@gmail.com>
2022-02-18 22:09:05 +0900

    Request permission & fetch weather data & apply ActivityIndication

--- App.js
+++ App.js
@@ -1,27 +1,54 @@
-import React from 'react';
-import { View, StyleSheet, Text, ScrollView, Dimensions } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Text, ScrollView, Dimensions, ActivityIndicator } from 'react-native';
+import * as Location from 'expo-location';
 
 const { width: SCREEN_WIDTH, height } = Dimensions.get('window');
+const API_KEY = 'f9671855cc1dad83ce46159fd131fc7f';
+
 
 export default function App () {
+  const [city, setCity] = useState('Loding...');
+  const [days, setDays] = useState([]);
+  const [location, setLocation] = useState();
+  const [ok, setOk] = useState(true);
+
+  const getWeather = async () => {
+    const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const [location] = await Location.reverseGeocodeAsync({latitude, longitude}, {useGoogleMaps: false});
+    setCity(location.city);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}`);
+    const json = await response.json();
+    // setDays(json.daily);
+  }
+
+  const getPermission = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+  }
+
+  useEffect(async () => {
+    await getWeather();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView horizontal pagingEnabled showsHorizontalScrollIndicator={false} contentContainerStyle={styles.weather}>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
+        {days.length ?
+          days.map((day, i) =>
+            <View style={styles.day} key={i}>
+              <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(2)}</Text>
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+            </View>) :
+            <View style={styles.day}>
+              <ActivityIndicator color='white' size='large' style={{marginTop: 10}}/>
+            </View>
+        }
+
       </ScrollView>
     </View>
   );
